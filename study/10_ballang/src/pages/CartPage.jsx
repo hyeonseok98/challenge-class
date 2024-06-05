d86d6830d4f54f06e593c19d93b69423d8e3e31c
hyeonseok98 <157561573+hyeonseok98@users.noreply.github.com>
2024-06-05 17:26:13 +0900

    update: study -> 10.ballang

---
+++ study/10_ballang/src/pages/CartPage.jsx
@@ -0,0 +1,48 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import api from "../api/api";
+
+function CartPage() {
+  const queryClient = useQueryClient();
+
+  const { data: cart, isLoading } = useQuery({
+    queryKey: ["cart"],
+    queryFn: () => api.cart.getCart(),
+  });
+
+  const { mutateAsync: removeItemFromCart } = useMutation({
+    mutationFn: (productId) => api.cart.removeItemFromCart(productId),
+  });
+
+  // 장바구니에서 제거하는 로직
+  const handleClickRemoveItemFromCart = (productId) => async () => {
+    await removeItemFromCart(productId);
+    alert("상품이 장바구니에서 제거되었습니다.");
+
+    queryClient.invalidateQueries({ queryKey: ["cart"] });
+  };
+
+  return (
+    <>
+      {isLoading ? (
+        "loading..."
+      ) : (
+        <ul style={{ textAlign: "left" }}>
+          {cart.items.map((cartItem) => (
+            <li key={cartItem.id}>
+              <h5>{cartItem.product.name}</h5>
+              <span>[{cartItem.quantity}]</span>
+              <button
+                onClick={handleClickRemoveItemFromCart(cartItem.product.id)}
+              >
+                장바구니에 빼기
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+      <h1>장바구니</h1>
+    </>
+  );
+}
+
+export default CartPage;
--- study/modal/src/App.jsx
+++ study/modal/src/App.jsx
@@ -1,4 +1,3 @@
-import { useScrollLock } from "@yoojinyoung/usescrolllock";
 import "./App.css";
 import { useModal } from "./contexts/modal.context";
 import "./custom.css";
@@ -9,11 +8,14 @@ function App() {
   const handleClickButton = () => {
     modal.open({ title: "제목", content: "콘텐츠" });
   };
+  const handleClickButton2 = () => {
+    modal.open({ title: "제목2", content: "콘텐츠" });
+  };
 
   return (
     <main>
       <button onClick={handleClickButton}>모달띄우기</button>
-
+      <button onClick={handleClickButton2}>모달띄우기2</button>
       {Array(5000)
         .fill(0)
         .map((_, index) => (
